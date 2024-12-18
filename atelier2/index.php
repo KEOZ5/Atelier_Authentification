3cbe1808b6528b0ece1abc6541414fd5257a0688
KEOZ5 <96393042+KEOZ5@users.noreply.github.com>
2024-12-18 11:17:12 +0100

    Update index.php

--- atelier2/index.php
+++ atelier2/index.php
@@ -5,10 +5,10 @@ session_start();
 // Vérifier si l'utilisateur est déjà en possession d'un cookie valide (cookie authToken ayant le contenu 12345)
 // Si l'utilisateur possède déjà ce cookie, il sera redirigé automatiquement vers la page home.php
 // Dans le cas contraire il devra s'identifier.
-if (isset($_COOKIE['authToken']) && str_starts_with($_COOKIE['authToken'],'12345')) {
+if (isset($_COOKIE['authToken']) && str_starts_with($_COOKIE['authToken'], 'admin_')) {
     header('Location: page_admin.php');
     exit();
-} else if (isset($_COOKIE['authToken']) && str_starts_with($_COOKIE['authToken'],'54321')) {
+} elseif (isset($_COOKIE['authToken']) && str_starts_with($_COOKIE['authToken'], 'user_')) {
     header('Location: page_user.php');
     exit();
 }
@@ -19,15 +19,16 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     $password = $_POST['password'];
 
     // Vérification simple du username et de son password.
-    // Si ok alors on initialise le cookie sur le poste de l'utilisateur 
-    $jeton = bin2hex(random_bytes(16));
+    // Si ok alors on initialise le cookie sur le poste de l'utilisateur
     if ($username === 'admin' && $password === 'secret') {
-        setcookie('authToken', "12345" . $jeton, time() + 60, '/', '', false, true); // Le Cookie est initialisé et valable pendant 1 heure (3600 secondes) 
+        $token = bin2hex(random_bytes(16));
+        setcookie('authToken', 'admin_' . $token, time() + 60, '/', '', true, true); // Le Cookie est initialisé et valable pendant 1 heure (3600 secondes)
         header('Location: page_admin.php'); // L'utilisateur est dirigé vers la page home.php
         exit();
-    } else if ($username === 'user' && $password === 'utilisateur') {
-        setcookie('authToken', "54321" . $jeton, time() + 60, '/', '', false, true); // Le Cookie est initialisé et valable pendant 1 heure (3600 secondes) 
-        header('Location: page_user.php'); // L'utilisateur est dirigé vers la page home.php
+    } elseif ($username === 'user' && $password === 'utilisateur') {
+        $token = bin2hex(random_bytes(16));
+        setcookie('authToken', 'user_' . $token, time() + 60, '/', '', true, true);
+        header('Location: page_user.php');
         exit();
     } else {
         $error = "Nom d'utilisateur ou mot de passe incorrect.";
@@ -55,6 +56,6 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
         <button type="submit">Se connecter</button>
     </form>
     <br>
-    <a href="../index.html">Retour à l'accueil</a>  
+    <a href="../index.html">Retour à l'accueil</a>
 </body>
 </html>
