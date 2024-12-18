43d52c124f0db8e25d75d40a2aed1acfc2663b4c
KEOZ5 <96393042+KEOZ5@users.noreply.github.com>
2024-12-18 11:04:28 +0100

    Update index.php

--- atelier2/index.php
+++ atelier2/index.php
@@ -5,8 +5,11 @@ session_start();
 // Vérifier si l'utilisateur est déjà en possession d'un cookie valide (cookie authToken ayant le contenu 12345)
 // Si l'utilisateur possède déjà ce cookie, il sera redirigé automatiquement vers la page home.php
 // Dans le cas contraire il devra s'identifier.
-if (isset($_COOKIE['authToken']) && $_COOKIE['authToken'] === '12345') {
+if (isset($_COOKIE['authToken']) && str_starts_with($_COOKIE['authToken'],'12345')) {
     header('Location: page_admin.php');
+    exit();
+} else if (isset($_COOKIE['authToken']) && str_starts_with($_COOKIE['authToken'],'54321')) {
+    header('Location: page_user.php');
     exit();
 }
 
@@ -17,9 +20,14 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
 
     // Vérification simple du username et de son password.
     // Si ok alors on initialise le cookie sur le poste de l'utilisateur 
+    $jeton = bin2hex(random_bytes(16));
     if ($username === 'admin' && $password === 'secret') {
-        setcookie('authToken', '12345', time() + 3600, '/', '', false, true); // Le Cookie est initialisé et valable pendant 1 heure (3600 secondes) 
+        setcookie('authToken', "12345" . $jeton, time() + 60, '/', '', false, true); // Le Cookie est initialisé et valable pendant 1 heure (3600 secondes) 
         header('Location: page_admin.php'); // L'utilisateur est dirigé vers la page home.php
+        exit();
+    } else if ($username === 'user' && $password === 'utilisateur') {
+        setcookie('authToken', "54321" . $jeton, time() + 60, '/', '', false, true); // Le Cookie est initialisé et valable pendant 1 heure (3600 secondes) 
+        header('Location: page_user.php'); // L'utilisateur est dirigé vers la page home.php
         exit();
     } else {
         $error = "Nom d'utilisateur ou mot de passe incorrect.";
